00a465e893697ddd6c7c99d0fda6fe9846115254
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-07-01 18:32:24 -0300

    Initial structure cypress

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    // baseUrl: "http://frontend:5173",
+    baseUrl: "http://localhost:5173/",
+    // baseUrl: "http://localhost:3000/",
+    supportFile: false,
+    specPattern: "cypress/e2e/**/*.cy.js",
+    screenshotOnRunFailure: true,
+    video: true,
+    defaultBrowser: "chrome",
+  },
+});
