5dca73ed07dac65d7112751403de2e2d876a1b02
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-07-17 16:30:31 -0300

    Refactoring

--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // baseUrl: "http://frontend:5173",
     baseUrl: "http://localhost:5173/",
-    // baseUrl: "http://localhost:3000/",
     supportFile: false,
     specPattern: "cypress/e2e/**/*.cy.js",
     screenshotOnRunFailure: false,
