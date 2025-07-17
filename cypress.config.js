122e85892a06a2663e9d7678e58d1fa1fb2e28ef
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-07-17 20:52:49 -0300

    Init browserstack

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
+const browserstackTestObservabilityPlugin = require('browserstack-cypress-cli/bin/testObservability/plugin');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config);
+      // require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config);
+      browserstackTestObservabilityPlugin(on, config);
+      return config;
     },
     baseUrl: "http://localhost:5173/",
     supportFile: false,
